20df5d5b1fe230a142823392395f2f282e1f6f95
Chicoo Freitas <chicoo.freitas84@gmail.com>
2024-03-14 14:51:08 -0300

    chore(api): create the api folder

---
+++ php/src/application/controllers/api/UserController.php
@@ -0,0 +1,14 @@
+<?php
+// * \Controllers\Api\UserController
+class UserController extends CI_Controller {
+
+    public function __contruct() {
+        parent::contruct();
+    }
+
+    public function index() {
+        $this->load->model('User_model');
+        $users = $this->User_model->get();
+        echo json_encode($users);
+    }
+}
